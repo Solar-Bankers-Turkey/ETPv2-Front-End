019521ff2a55d3ec6075acbf164b83023259ebe4
Ivy Ti <chivy1221@gmail.com>
2020-05-31 23:34:16 +0300

    updated analytics

--- etp/src/app/services/data.service.ts
+++ etp/src/app/services/data.service.ts
@@ -1,6 +1,26 @@
-import { Injectable } from '@angular/core';
-import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { Injectable, ViewChild } from '@angular/core';
+import {
+  ChartComponent,
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexDataLabels,
+  ApexTitleSubtitle,
+  ApexStroke,
+  ApexGrid,
+  ApexTooltip,
+} from 'ng-apexcharts';
 
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  dataLabels: ApexDataLabels;
+  grid: ApexGrid;
+  stroke: ApexStroke;
+  title: ApexTitleSubtitle;
+  tooltip: ApexTooltip;
+};
 @Injectable({
   providedIn: 'root',
 })
@@ -29,191 +49,106 @@ export class DataService {
     },
   ];
 
-  //Room data
-  iotData = [
-    {
-      series: [12],
-      fill: {
-        type: 'solid',
-        colors: ['#05C985'],
-      },
-      room: 'Living Room',
-      distance: '1034KM',
-    },
-    {
-      series: [16],
-      fill: {
-        type: 'solid',
-        colors: ['#FFAB4F'],
-      },
-      room: 'Studio',
-      distance: '1034KM',
-    },
-    {
-      series: [32],
-      fill: {
-        type: 'solid',
-        colors: ['#EE423D'],
-      },
-      room: "Kid's room",
-      distance: '1034KM',
-    },
-    {
-      series: [18],
-      fill: {
-        type: 'solid',
-        colors: ['#843FA0'],
-      },
-      room: 'Kitchen',
-      distance: '1034KM',
-    },
-  ];
-
   //line chart options
-  multi = [
-    {
-      name: 'Living Room',
-      series: [
-        {
-          name: 'Monday',
-          value: 100,
-        },
-        {
-          name: 'Tuesday',
-          value: 400,
-        },
-        {
-          name: 'Wednessday',
-          value: 500,
-        },
-        {
-          name: 'Thursday',
-          value: 300,
-        },
-        {
-          name: 'Friday',
-          value: 200,
-        },
-        {
-          name: 'Saturday',
-          value: 500,
-        },
-        {
-          name: 'Sunday',
-          value: 400,
-        },
-      ],
-    },
-    {
-      name: 'Studio',
-      series: [
-        {
-          name: 'Monday',
-          value: 50,
-        },
-        {
-          name: 'Tuesday',
-          value: 200,
-        },
-        {
-          name: 'Wednessday',
-          value: 100,
-        },
-        {
-          name: 'Thursday',
-          value: 300,
-        },
-        {
-          name: 'Friday',
-          value: 200,
-        },
-        {
-          name: 'Saturday',
-          value: 600,
-        },
-        {
-          name: 'Sunday',
-          value: 400,
-        },
-      ],
-    },
-    {
-      name: 'Kids Room',
-      series: [
-        {
-          name: 'Monday',
-          value: 300,
-        },
-        {
-          name: 'Tuesday',
-          value: 100,
-        },
-        {
-          name: 'Wednessday',
-          value: 500,
-        },
-        {
-          name: 'Thursday',
-          value: 300,
-        },
-        {
-          name: 'Friday',
-          value: 200,
-        },
-        {
-          name: 'Saturday',
-          value: 100,
-        },
-        {
-          name: 'Sunday',
-          value: 200,
-        },
-      ],
-    },
-    {
-      name: 'Kitchen',
-      series: [
-        {
-          name: 'Monday',
-          value: 200,
-        },
-        {
-          name: 'Tuesday',
-          value: 100,
-        },
-        {
-          name: 'Wednessday',
-          value: 200,
-        },
-        {
-          name: 'Thursday',
-          value: 300,
-        },
-        {
-          name: 'Friday',
-          value: 100,
-        },
-        {
-          name: 'Saturday',
-          value: 500,
-        },
-        {
-          name: 'Sunday',
-          value: 400,
-        },
-      ],
-    },
-  ];
 
-  showLabels: boolean = true;
-  animations: boolean = true;
-  xAxis: boolean = true;
-  yAxis: boolean = true;
-  timeline: boolean = true;
-  yScaleMax = 700;
-  yScaleMin = 0;
-  colorScheme = {
-    domain: ['#05C985', '#FFAB4F', '#EE423D', '#843FA0'],
+  @ViewChild('chart') chart: ChartComponent;
+  chartOptions: Partial<ChartOptions> = {
+    series: [
+      { data: [100, 400, 500, 300, 200, 500, 400], name: 'Living Room' },
+      { data: [50, 200, 100, 300, 200, 600, 400], name: 'Studio' },
+
+      {
+        data: [300, 120, 503, 330, 200, 230, 120],
+        name: "Kid's room",
+      },
+      {
+        data: [201, 130, 350, 200, 444, 244, 300],
+        name: 'Kitchen',
+      },
+    ],
+    chart: {
+      type: 'line',
+      zoom: {
+        enabled: false,
+      },
+      toolbar: {
+        show: false,
+      },
+      foreColor: '#869AAC',
+    },
+    tooltip: {
+      theme: 'dark',
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      curve: 'straight',
+      width: 1.9,
+    },
+    grid: {
+      xaxis: {
+        lines: {
+          show: true,
+        },
+      },
+      borderColor: '#20293c',
+    },
+    xaxis: {
+      categories: [
+        'Monday',
+        'Tuesday',
+        'Wednessday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+        'Sunday',
+      ],
+      axisBorder: {
+        show: true,
+        color: '#20293c',
+      },
+      axisTicks: {
+        show: false,
+      },
+      crosshairs: {
+        show: true,
+        width: 1,
+        position: 'back',
+        opacity: 0.9,
+        stroke: {
+          color: '#869AAC',
+          width: 1,
+          dashArray: 0,
+        },
+        fill: {
+          type: 'solid',
+          color: 'red',
+        },
+      },
+    },
   };
+
+  sparklineOptions: Partial<ChartOptions> = {
+    chart: {
+      type: 'line',
+      zoom: {
+        enabled: false,
+      },
+      toolbar: {
+        show: false,
+      },
+      sparkline: {
+        enabled: true,
+      },
+      height: 40,
+    },
+    stroke: {
+      width: 1.9,
+    },
+  };
+
+  domain = ['#05C985', '#FFAB4F', '#EE423D', '#843FA0'];
+
   constructor() {}
 }
--- etp/src/app/services/settings.service.ts
+++ etp/src/app/services/settings.service.ts
@@ -25,6 +25,71 @@ export type ChartOptions = {
   providedIn: 'root',
 })
 export class SettingsService {
+  //Room data
+  iotData = [
+    {
+      series: [174],
+      sparkline: {
+        data: [100, 400, 500, 300, 200, 500, 400],
+        name: 'Living Room',
+      },
+
+      fill: {
+        type: 'solid',
+        colors: ['#05C985'],
+      },
+      room: 'Living Room',
+      distance: '1034KM',
+    },
+    {
+      series: [201],
+      sparkline: {
+        data: [50, 200, 100, 300, 200, 600, 400],
+        name: 'Studio',
+      },
+      fill: {
+        type: 'solid',
+        colors: ['#FFAB4F'],
+      },
+      room: 'Studio',
+      distance: '1034KM',
+    },
+    {
+      series: [289],
+      sparkline: {
+        data: [201, 130, 350, 200, 444, 244, 300],
+        name: 'Kitchen',
+      },
+      fill: {
+        type: 'solid',
+        colors: ['#EE423D'],
+      },
+      room: "Kid's room",
+      distance: '1034KM',
+    },
+    {
+      series: [196],
+      sparkline: {
+        data: [300, 120, 503, 330, 200, 230, 120],
+        name: "Kid's room",
+      },
+      fill: {
+        type: 'solid',
+        colors: ['#843FA0'],
+      },
+      room: 'Kitchen',
+      distance: '1034KM',
+    },
+  ];
+
+  total = this.iotData
+    .map((x) => {
+      return x.series[0];
+    })
+    .reduce((a, c) => {
+      return a + c;
+    });
+
   // breakpoints
   @ViewChild('chart') chart: ChartComponent;
   public chartOptions: Partial<ChartOptions> = {
@@ -109,6 +174,14 @@ export class SettingsService {
       shareReplay()
     );
 
+  isMedium$: Observable<boolean> = this.breakpointObserver
+    .observe([Breakpoints.TabletPortrait])
+    .pipe(
+      map((result) => {
+        return result.matches;
+      }),
+      shareReplay()
+    );
   // classes
   sidetoggle = '';
   overlay = 'overlay';
